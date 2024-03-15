36789e861f6f637f19420ec9af8a4df36c664787
Rychard2107 <zafler2107@gmail.com>
2024-03-14 23:25:39 -0300

    get competition

--- app_fute/views.py
+++ app_fute/views.py
@@ -10,11 +10,22 @@ def home(request):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
-        return render(request, 'app_fute/pages/home.html', {'data': data})
+
+        competitions = []
+
+        for competition in data['competitions']:
+            name = competition['name']
+            id = competition['id']
+            competitions.append({'name': name, 'id': id})
+
+        return render(request, 'app_fute/pages/home.html', {'competitions': competitions})
     except requests.exceptions.RequestException as e:
+        # Erro de requisição
         error_message = f"Seu erro foi amar demais, e mais esse aqui também: {str(e)}"
+
         return render(request, 'app_fute/pages/error.html', {'error_message': error_message})
     except Exception as e:
         # Erros gerais
         error_message = f"Este erro foi inesperado: {str(e)}"
+
         return render(request, 'app_fute/pages/error.html', {'error_message': error_message})
