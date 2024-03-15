b01b513a47cc596d13f0800a51b0817c5036dc25
Rychard2107 <zafler2107@gmail.com>
2024-03-14 21:50:51 -0300

    initial front-end

--- app_fute/models.py
+++ app_fute/models.py
@@ -1,8 +1,25 @@
-from django.db import models
+""" from django.db import models
 
 
-class Times(models.Model):
-    nome_time = models.CharField(max_length=50)
-    
+class Competition(models.Model):
+    name = models.CharField(max_length=30)
+    match = models.ForeignKey('Match')
+
     def __str__(self):
         return self.name
+
+
+class Match(models.Model):
+    name = models.CharField(max_length=30)
+    team = models.ManyToManyField('Team', related_name='teams')
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+ """
--- app_fute/views.py
+++ app_fute/views.py
@@ -1,9 +1,20 @@
+from django.shortcuts import render
 import requests
+# from .models import Competition
 
 
-url = 'https://api.football-data.org/v4/teams/'
-headers = {'X-Auth-Token': '02dbfc8a7b1f4f6f9bd8817070cce254'}
-
-response = requests.get(url, headers=headers)
-for Teams in response.json()['teams']:
-    print(Teams)
+def home(request):
+    try:
+        url = 'https://api.football-data.org/v4/competitions/'
+        headers = {'X-Auth-Token': '02dbfc8a7b1f4f6f9bd8817070cce254'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return render(request, 'app_fute/pages/home.html', {'data': data})
+    except requests.exceptions.RequestException as e:
+        error_message = f"Seu erro foi amar demais, e mais esse aqui também: {str(e)}"
+        return render(request, 'app_fute/pages/error.html', {'error_message': error_message})
+    except Exception as e:
+        # Erros gerais
+        error_message = f"Este erro foi inesperado: {str(e)}"
+        return render(request, 'app_fute/pages/error.html', {'error_message': error_message})
